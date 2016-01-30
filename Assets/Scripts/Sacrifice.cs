4e1a9e0ff76e6c2f860c84cc392d930ca9b80fd1
xcissors <xcissors@gmail.com>
2016-01-30 14:21:20 -0800

    quick sacrifice code

--- Assets/Scripts/Sacrifice.cs
+++ Assets/Scripts/Sacrifice.cs
@@ -5,9 +5,26 @@ public class Sacrifice : MonoBehaviour {
 
 	public GameObject m_Sacrifice;
 	private GameObject mOwner;
+    public string sacrificeType;
+
 
 	public void Respawn() {
 		Instantiate (m_Sacrifice);
+        int randomNumber = Random.Range(0, 2);
+        switch (randomNumber) {
+            case 0:
+                sacrificeType = "Lamb";
+                break;
+            case 1:
+                sacrificeType = "Chicken";
+                break;
+            case 2:
+                sacrificeType = "Totem";
+                break;
+            default:
+                sacrificeType = "Lamb";
+                break;
+        }
 	}
 
 	public void SetOwner(GameObject owner) {
