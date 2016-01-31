f2dce315ba71f75d163273b455e97cbf3ee42c82
xcissors <xcissors@gmail.com>
2016-01-31 00:28:43 -0800

    Collision Box Updates, AI compatibility

--- Assets/Scripts/AIPlayer.cs
+++ Assets/Scripts/AIPlayer.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class AIPlayer : MonoBehaviour {
-
-    private float _fCurrentMoveSpeed = 1;
-    private float _waitTime = 0.0f;
+public class AIPlayer : Player {
+    
     public float _offset = 0.3f;
-    public float _fMoveSpeed = 1;
-    public float _fSlowPlayerTimer = 3.0f;
-    public float _fStunPlayerTimer = 3.0f;
+
+    private GameObject m_Sacrifice;
+
+    private Sprite m_IdleSprite;
+    private Sprite m_AttackSprite;
+
+
+    private bool m_IsAttacking;
+    private float m_AnimationTimer;
+    #region Power States
 
     public GameObject m_sacrifice;
     public GameObject m_base;
@@ -16,17 +23,35 @@ public class AIPlayer : MonoBehaviour {
     private Rigidbody2D _rigidbody;
     public int restrictMovement;
     public Vector2 finalPos;
-    private float lastMoveHor;
-    private float lastMoveVer;
-    public string movetyp;
-    public float test;
+    private string movetyp;
+    private float test;
 
+    private Animator animator;
+    #endregion
 
     // Use this for initialization
     void Start () {
         _rigidbody = GetComponent<Rigidbody2D>();
         restrictMovement = 0;
+        m_IdleSprite = GetComponent<SpriteRenderer> ().sprite;
+		m_AttackSprite = Resources.Load("battingv1", typeof(Sprite)) as Sprite;
+
+		m_DebuffTimers = new Dictionary<Debuffs, float>();
+		m_PowerStateTimers = new Dictionary<PowerStates, float>();
+
+		m_DebuffTimers.Add(Debuffs.Slow, 0);
+		m_DebuffTimers.Add(Debuffs.Stun, 0);
+
+		m_PowerStateTimers.Add(PowerStates.Invulnerability, 0);
+		m_PowerStateTimers.Add(PowerStates.SuperSpeed, 0);
+        
+		PlayerMovement control = gameObject.GetComponent<PlayerMovement>();
+		control._fMoveSpeed = m_MovementSpeed;
+
+		animator = GetComponent<Animator> ();
     }
+
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -125,55 +150,14 @@ public class AIPlayer : MonoBehaviour {
         destinationx = finalPos.x;
         destinationy = finalPos.y;
 
-        moveTowards(destinationx, destinationy);
-
-
-    }
-
-    public void moveTowards(float gotoX, float gotoY) {
-        Transform _transform = GetComponent<Transform>();
-        Vector2 _position = _transform.position;
-        float horVar = 0;
-        float vertVar = 0;
-        Vector2 displacement = _position -  new Vector2 (gotoX, gotoY);
-        Vector2 displacementN = new Vector2(gotoX, gotoY) - _position;
-        displacementN = (displacementN * 0.5f) + (new Vector2(lastMoveHor, lastMoveVer) * 0.5f);
-
-        horVar = displacementN.x;
-        vertVar = displacementN.y;
-
-
-        AIUpdate(horVar, vertVar);
-    }
-
-    //Updates movement, rotation.
-    public void AIUpdate(float hor, float ver)
-    {
-        if (Random.Range(0, 100) < 20)
-        {
-
-            print("stop");
-            _movement = new Vector2(0, 0);
-        }
-        else
-        {
-            _movement = new Vector2(hor, ver);
-            _movement.Normalize();
-        }
-
-        _rigidbody.velocity = _movement * _fMoveSpeed;
-        if (lastMoveHor != hor || lastMoveVer != ver)
-        {
-            makeDecision();
-        }
-
-        lastMoveHor = hor;
-        lastMoveVer = ver;
         
-        float angle = Mathf.Atan2(-hor, ver) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-        
+
+        AIPlayerMovement movement = GetComponent<AIPlayerMovement>();
+        movement.moveTowards(destinationx, destinationy);
+
     }
+
+
 
     //private void moveWait()
     //{
@@ -188,7 +172,7 @@ public class AIPlayer : MonoBehaviour {
     //    restrictMovement = 0;
     //}
 
-    private void makeDecision()
+    public void makeDecision()
     {
         StartCoroutine(makeDecisionTimer());
     }
@@ -200,41 +184,5 @@ public class AIPlayer : MonoBehaviour {
         yield return new WaitForSeconds(randomNumber);
         restrictMovement = 0;
     }
-
-
-    private void SlowPlayer()
-    {
-        StartCoroutine(SlowPlayerTimer());
-    }
-
-    IEnumerator SlowPlayerTimer()
-    {
-        _fCurrentMoveSpeed /= 2;
-        yield return new WaitForSeconds(_fSlowPlayerTimer);
-        _fCurrentMoveSpeed *= 2;
-    }
-
-    private void StunPlayer()
-    {
-        StartCoroutine(StunPlayerTimer());
-    }
-
-    IEnumerator StunPlayerTimer()
-    {
-        _fCurrentMoveSpeed = 0;
-        yield return new WaitForSeconds(_fStunPlayerTimer);
-        _fCurrentMoveSpeed = _fMoveSpeed;
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.name == "slow-box")
-        {
-            SlowPlayer();
-        }
-        else if (other.gameObject.name == "stun-box")
-        {
-            StunPlayer();
-        }
-    }
+    
 }
