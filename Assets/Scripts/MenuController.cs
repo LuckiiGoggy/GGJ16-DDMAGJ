7565b978d805ec5b057854ace40de9a6b25a4443
Jack Tsai <jtsai0606@gmail.com>
2016-01-31 10:31:42 -0800

    fixed menu screen controls

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -10,8 +10,10 @@ public class MenuController : MonoBehaviour {
 	// 1 = controller1
 	// etc.
 	public int _iControllerNumber;
+	private InputControllerManager.KeyMap input;
 
 	public List<MenuTotemPart> m_MenuTotemParts;
+
 
 	private float m_PreviousAxis;
 	private int m_Current;
@@ -20,26 +22,26 @@ public class MenuController : MonoBehaviour {
 	{
 		// then tell the inputcontrollermanager to set its static variables to match
 		// THIS script's _iControllerNumber,
-		InputControllerManager.WhatControllerAmI (_iControllerNumber);
+		input = InputControllerManager.WhatControllerAmI (_iControllerNumber);
 	}
 
 	void Update ()
 	{
-		if (Input.GetKey(InputControllerManager._button01)) {
+		if (Input.GetKey(input._button01)) {
 			print ("start game");
 			Application.LoadLevel("Scene01");
 		}
 
-		if (Input.GetAxisRaw(InputControllerManager._verticalAxis) > 0 && !(m_PreviousAxis > 0)) {
+		if (Input.GetAxisRaw(input._verticalAxis) > 0 && !(m_PreviousAxis > 0)) {
 			m_Current = Math.Min(++m_Current, m_MenuTotemParts.Count - 1);
 		}
 
-		if (Input.GetAxisRaw(InputControllerManager._verticalAxis) < 0 && !(m_PreviousAxis < 0)) {
+		if (Input.GetAxisRaw(input._verticalAxis) < 0 && !(m_PreviousAxis < 0)) {
 			m_Current = Math.Max(--m_Current, 0);
 		}
 
 		m_MenuTotemParts [m_Current].OnMouseOver ();
 
-		m_PreviousAxis = Input.GetAxisRaw (InputControllerManager._verticalAxis);
+		m_PreviousAxis = Input.GetAxisRaw (input._verticalAxis);
 	}
 }
