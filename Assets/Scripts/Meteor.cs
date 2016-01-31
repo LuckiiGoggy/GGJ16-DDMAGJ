e8ece559ecb24569a0f82843c1b24ca024e86512
LuckiiGoggy <luck-13@live.ca>
2016-01-31 13:14:01 -0800

    Meteor stuff working

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,17 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Meteor : MonoBehaviour {
+public class Meteor : MonoBehaviour
+{
 
-    public float m_TimeAlive;
+    public Vector2 toBeSize;
+    public float m_timeToChangeSize;
 
-	// Use this for initialization
-	void Start () {
-	    Destroy(this.gameObject, m_TimeAlive);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float m_enableColliderAfterTime;
+
+    public float m_deathTimer;
+
+    public CircleCollider2D collider;
+
+    void Start()
+    {
+        Destroy(gameObject, m_deathTimer);
+
+        collider = GetComponent<CircleCollider2D>();
+        StartCoroutine(EnableColliderAfterTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector2.Lerp(transform.localScale, toBeSize, m_timeToChangeSize);
+    }
+
+    IEnumerator EnableColliderAfterTime()
+    {
+        yield return new WaitForSeconds(m_enableColliderAfterTime);
+        collider.enabled = true;
+    }
 }
